d208808be9cbbc73a30d4670bd7a551b7c16a127
Shoumo Shahriar Araf <shahriaraf01@gmail.com>
2026-01-14 11:48:00 +0600

    fixed module

--- src/proposals/proposals.module.ts
+++ src/proposals/proposals.module.ts
@@ -4,11 +4,15 @@ import { ProposalsController } from './proposals.controller';
 import { ProposalsService } from './proposals.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Proposal, ProposalSchema } from './schemas/proposal.schema';
+// 👇 1. Import User Schema
+import { User, UserSchema } from '../users/schemas/user.schema';
 
 @Module({
     imports: [
       MongooseModule.forFeature([
         { name: Proposal.name, schema: ProposalSchema },
+        // 👇 2. Register User Schema here so ProposalsService can use it
+        { name: User.name, schema: UserSchema }, 
       ]),
     ],
   controllers: [ProposalsController],
